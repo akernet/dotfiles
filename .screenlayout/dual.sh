b39428536988938bc70cd41ec3b8f46a176dec32
Jens Åkerlund <jens@akernet.se>
2017-08-22 21:11:33 +0200

    Initial commit
    Added Manjaro based dotfiles for terminal variables and colors as well as
    xorg screen layouts.

---
+++ .screenlayout/dual.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output DisplayPort-2 --off --output DVI-D-1 --primary --mode 1920x1080 --rate 120 --pos 1080x376 --rotate normal --output HDMI-1-1 --mode 1920x1080 --pos 0x0 --rotate left --output HDMI-1-2 --off --output VGA-1-1 --off --output DVI-I-0 --off
---
+++ .screenlayout/tv.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DisplayPort-1 --off --output DisplayPort-0 --off --output DisplayPort-2 --off --output DVI-D-1 --off --output HDMI-1-1 --off --output HDMI-1-2 --mode 1920x1080 --pos 600x320 --rotate normal --output VGA-1-1 --off --output DVI-I-0 --off
